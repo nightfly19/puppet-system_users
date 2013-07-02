eefb46dbf063120b386f04d6ac702e7be3d5127a
Sage Imel <sage@sagenite.net>
2013-07-02 01:59:11 -0700

    Updated for new parser

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,30 +1,18 @@
 class system_users(
-  $users,
-  $managehome         = true,
-  $homes              = '/home',
+  $users              = [],
+  $user_defaults      = {},
+  $key_defaults       = {},
   $user_resource_type = 'user',
   $key_resource_type  = 'ssh_authorized_key'
 ) {
-
-  $group_aliases = {
-    'wheel' => $operatingsystem ? {
-      'freebsd' => 'wheel',
-      default   => 'root',
-    },
-  }
-
-  $shell_aliases = {
-    'bash' => $operatingsystem ? {
-      'freebsd' => '/usr/local/bin/bash',
-      default   => '/bin/bash',
-    },
-    'zsh' => $operatingsystem ? {
-      'freebsd' => '/usr/local/bin/zsh',
-      default   => '/bin/zsh',
-    },
-  }
-
-  if $users != undef {
-    create_resources('system_users::user', $users)
+  each($users) { |$username, $values|
+    $user = {"${username}" => $values}
+    create_resources($user_resource_type, $user, $user_defaults)
+    if $values['keys'] {
+      each($values['keys']) { |$keyname, $values|
+        $key = {"${username}-${keyname}" => merge($values, {user => "${username}"})}
+        create_resources($key_resource_type, $key, $key_defaults)
+      }
+    }
   }
 }
